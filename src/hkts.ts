01183b7e1fb0e9fef0f91da9950ecfc91c9563d8
mattmark <matt.vandusen@float-left.co>
2018-08-16 18:40:17 -0500

    fix(Module): Fix missing Module export

--- src/hkts.ts
+++ src/hkts.ts
@@ -1,6 +1,7 @@
 import * as Continuation from "./Continuation"
 import * as Applicative from "./Applicative"
 import * as Identity from "./Identity"
+import * as Compose from "./Compose"
 import * as Either from "./Either"
 import * as Chain from "./Chain"
 import * as Maybe from "./Maybe"
@@ -8,4 +9,4 @@ import * as Monad from "./Monad"
 import * as List from "./List"
 import * as Pipe from "./Pipe"
 
-export { Continuation, Applicative, Identity, Either, Chain, Maybe, Monad, List, Pipe }
+export { Continuation, Applicative, Identity, Either, Chain, Maybe, Monad, List, Pipe, Compose }
